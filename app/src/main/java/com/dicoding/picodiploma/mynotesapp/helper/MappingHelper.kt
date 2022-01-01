de4356a748fb3178634dfffadd883f1eff0f8a84
Zaenurrochman <zaenur.rochman98@gmail.com>
2022-01-01 20:43:13 +0700

    add crud with sqlite

---
+++ app/src/main/java/com/dicoding/picodiploma/mynotesapp/helper/MappingHelper.kt
@@ -0,0 +1,21 @@
+package com.dicoding.picodiploma.mynotesapp.helper
+
+import android.database.Cursor
+import com.dicoding.picodiploma.mynotesapp.db.DatabaseContract
+import com.dicoding.picodiploma.mynotesapp.entity.Note
+
+object MappingHelper {
+    fun mapCursorToArrayList(notesCursor: Cursor?): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+}
